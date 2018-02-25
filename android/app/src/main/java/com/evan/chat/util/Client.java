4e33468cc9207305c23e66f430d03a3b3b5d9315
evan-yjq <yjq877978471@gmail.com>
2018-02-25 16:59:38 +0800

    转移

---
+++ android/app/src/main/java/com/evan/chat/util/Client.java
@@ -0,0 +1,93 @@
+package com.evan.chat.util;
+
+import android.os.Bundle;
+import android.os.Message;
+
+import java.io.*;
+import java.net.Socket;
+
+import static com.evan.chat.activity.Data.socket;
+import static com.evan.chat.activity.Data.socketl;
+
+/**
+ * Created by yejiaquan on 12/27/16.
+ */
+//客户端
+public class Client implements Serializable {
+
+    private PrintWriter pw;
+    private BufferedReader br;
+
+    public Client(){
+        try {
+            socket=new Socket("115.28.216.244",10086);
+            socketl=new Socket("115.28.216.244",10086);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Message login(String username, String password) {
+        String result = "false";
+        String id = "";
+        try {
+            //2、获取输出流，向服务器端发送信息
+            pw = new PrintWriter(socket.getOutputStream());//将输出流包装成打印流
+            pw.write("login-" + username + "-" + password + "\nend\n");
+            pw.flush();
+            //3、获取输入流，并读取服务器端的响应信息
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String info;
+            while (true) {
+                info = br.readLine();
+                if (info != null && !"end".equals(info)) {
+                    id = info;
+                    break;
+                }
+            }
+            String[] infos = id.split("-");
+            if ("true".equals(infos[0])) {
+                id = infos[1];
+                result = "true";
+            }
+        } catch (IOException e) {
+            System.out.println("Client login fail");
+        }
+        Message msg = new Message();
+        Bundle b = new Bundle();
+        b.putString("result", result);
+        b.putString("id", id);
+        msg.setData(b);
+        return msg;
+    }
+
+    public Message get(String content) {
+        String result = "";
+        String key = content.split("-")[0];
+        try {
+            //2、获取输出流，向服务器端发送信息
+            pw = new PrintWriter(socket.getOutputStream());//将输出流包装成打印流
+            pw.write(content + "\nend\n");
+            pw.flush();
+            if (!key.equals("chat")) {
+                //3、获取输入流，并读取服务器端的响应信息
+                br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String info;
+                while (true) {
+                    info = br.readLine();
+                    if (info != null && !"end".equals(info)) {
+                        result = info;
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Client " + key + " fail");
+        }
+        Message msg = new Message();
+        Bundle b = new Bundle();
+        b.putString("result", result);
+        msg.setData(b);
+        return msg;
+    }
+}
---
+++ android/app/src/main/java/com/evan/chat/util/SetImageButton.java
@@ -0,0 +1,83 @@
+package com.evan.chat.util;
+
+
+import com.evan.chat.view.*;
+
+public class SetImageButton {
+    public static boolean setDialogButton(DialogButton button,String content,int user,Integer userHead){
+        button.setContent(content);
+        button.setUser(user);
+        if(userHead!=null)
+        button.setUserHead(userHead);
+        return true;
+    }
+
+    public static boolean setTopTitleButton(TopTitleButton button, int back, Object title, int more){
+        button.setBack(back);
+        button.setMore(more);
+        switch (title.getClass().getSimpleName()) {
+            case "String":
+                button.setTitle((String) title);
+                return true;
+            case "Integer":
+                button.setTitle((int) title);
+                return true;
+            default:
+                button.setTitle("");
+                return false;
+        }
+    }
+
+//    public static boolean setHeadLineButton(SmallTitleButton button, Object title, Object seeMore, int arrow){
+////        button.setImgTitle(imgTitle);
+//        if(title.getClass().getSimpleName().equals("String")){
+//            button.setTitle((String)title);
+//        }else if(title.getClass().getSimpleName().equals("Integer")){
+//            button.setTitle((int)title);
+//        }else{
+//            button.setTitle("");
+//        }
+//        if(seeMore.getClass().getSimpleName().equals("String")){
+//            button.setSeeMore((String)seeMore);
+//        }else if(seeMore.getClass().getSimpleName().equals("Integer")){
+//            button.setSeeMore((int)seeMore);
+//        }else{
+//            button.setSeeMore("");
+//        }
+//        button.setArrow(arrow);
+//        return true;
+//    }
+
+    public static boolean setImageTextButton(ImageTextButton button, Object text, int image, Integer textColor){
+        switch (text.getClass().getSimpleName()) {
+            case "String":
+                button.setText((String) text);
+                break;
+            case "Integer":
+                button.setText((int) text);
+                break;
+            default:
+                button.setText("");
+                break;
+        }
+        button.setImgResource(image);
+        if(textColor!=null)
+        button.setTextColor(textColor);
+        return true;
+    }
+
+    public static boolean setFriendsButton(FriendsButton button, int userHead, String username, String signature, String state){
+        button.setUserHead(userHead);
+        button.setUsername(username);
+        button.setSignature(signature);
+        button.setState(state);
+        return true;
+    }
+
+//    public static boolean setBBSLineButton(BBSLineButton button, int img, String title, String content){
+//        button.setImg(img);
+//        button.setTitle(title);
+//        button.setContent(content);
+//        return true;
+//    }
+}
